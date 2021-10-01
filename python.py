3d6585042da512d912d8de335d675cedc8d537b5
safarafa <tpserafin@wp.pl>
2021-10-01 10:24:47 +0200

    Python starter with some assert

---
+++ python.py
@@ -0,0 +1,16 @@
+#python 3.6.9
+
+# ======================= CODE HERE =======================
+ 
+def calculate(a,b):
+    return 0
+
+
+# ======================= TESTS =======================
+assert calculate(10,10) == 0, "Test if 10 * 10 equals 100"
+
+# ======================= USE =======================
+
+a = float(input())
+b = int(input())
+print(calculate(a,b))
